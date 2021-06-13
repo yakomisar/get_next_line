21d808873f984c1e8260cf48f0427e60db8116ef
Jennine Macmillan <jmacmill@oa-d5.msk.21-school.ru>
2021-06-13 14:01:11 +0300

    asd

--- get_next_line.c
+++ get_next_line.c
@@ -1,90 +1,77 @@
 #include "get_next_line.h"
 #include <stdio.h>
 
-
 char	*check_remainder(char **str, char **line)
 {
-    char	*result;
+	char		*result;
 
-    result = NULL;
-    if (*str)
-    {
-        if ((result = ft_strchr(*str, '\n')))
-        {
-            *result = '\0';
-            result++;
-            *line = ft_strdup(*str);
-            *str = ft_strdup(result);
-        }
-        else
-        {
-            *line = ft_strdup(*str);
-            ft_strclr(*str);
-        }
-    }
-    else
-    {
-        *line = ft_strnew(1);
-    }
-    return (result);
+	result = NULL;
+	if (*str)
+	{
+		result = ft_strchr(*str, '\n');
+		if (result)
+		{
+			*result = '\0';
+			result++;
+			*line = ft_strdup(*str);
+			*str = ft_strdup(result);
+		}
+		else
+		{
+			*line = ft_strdup(*str);
+			ft_strclr(*str);
+		}
+	}
+	else
+	{
+		*line = ft_strnew(1);
+	}
+	return (result);
 }
 
 int	get_next_line(int fd, char **line)
 {
-    int			res;
-    char		buf[BUF_SIZE + 1];
-    static char	*str;
-    char        *position_n;
+	int			res;
+	char		buf[BUF_SIZE + 1];
+	static char	*str;
+	char		*position_n;
+    char        *temp;
 
-    if (fd < 0 || line == NULL)
-        return (-1);
-    position_n = check_remainder(&str, line);
+	if (fd < 0 || line == NULL)
+		return (-1);
+	position_n = check_remainder(&str, line);
     while (!position_n && (res = read(fd, buf, BUF_SIZE)))
-    {
-        buf[res] = '\0';
-        if ((position_n = ft_strchr(buf, '\n')))
-        {
-            *position_n = '\0';
-            position_n++;
-            str = ft_strdup(position_n);
-        }
-        *line = ft_strjoin(*line, buf);
-    }
-    return (0);
+	{
+		buf[res] = '\0';
+		position_n = ft_strchr(buf, '\n');
+		if (position_n)
+		{
+			*position_n = '\0';
+			position_n++;
+			str = ft_strdup(position_n);
+		}
+        temp = *line;
+		*line = ft_strjoin(*line, buf);
+        free(temp);
+	}
+	if (ft_strlen(*line) || res)
+		return (1);
+	else
+		return (0);
 }
 
 int	main(void) {
     int		fd;
-    int		ret;
     char    *line;
-
-    fd = open("/Users/olegkomisarenko/Desktop/42/get_next_line/sample.txt", O_RDONLY);
+    
+    fd = open("/Users/jmacmill/Desktop/42/gnl/sample.txt", O_RDONLY);
     if (fd == -1)
     {
         printf("open() unable to open the file");
         return (1);
     }
-    get_next_line(fd, &line);
-    printf("Result #1 : %s\n", line);
-
-    get_next_line(fd, &line);
-    printf("Result #2 : %s\n", line);
-
-    get_next_line(fd, &line);
-    printf("Result #3 : %s\n", line);
-
-    get_next_line(fd, &line);
-    printf("Result #4 : %s\n", line);
-
-	get_next_line(fd, &line);
-    printf("Result #5 : %s\n", line);
-
-	get_next_line(fd, &line);
-    printf("Result #6 : %s\n", line);
-
-	get_next_line(fd, &line);
-    printf("Result #7 : %s\n", line);
-
+    while ((get_next_line(fd, &line)))
+        printf("Result # : %s\n", line);
     if (close(fd) == -1)
     {
         printf("close() error");
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,24 +1,5 @@
 #include "get_next_line.h"
 
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
-
-void	ft_putstr(char *s)
-{
-	int	i;
-
-	if (s == NULL)
-		return ;
-	i = 0;
-	while (s[i] != '\0')
-	{
-		ft_putchar(s[i]);
-		i++;
-	}
-}
-
 size_t	ft_strlen(const char *str)
 {
 	size_t	i;
